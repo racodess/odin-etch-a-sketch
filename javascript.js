998aa8f1a760abd400d2c34d0c932167c0090e56
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-02 17:55:05 -0400

    Add row element, append to .sketch, wrap square logic within inner for loop

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
 const sketch = document.querySelector("#sketch-container");
 
 for (let i = 0; i < 16; i++) {
-  const square = document.createElement("div");
+  const row = document.createElement("div");
 
-  square.setAttribute("class", "square");
-  square.style.minHeight = "30px";
-  square.style.maxWidth = "30px";
+  sketch.appendChild(row);
 
-  sketch.appendChild(square);
+  for (let i = 0; i < 16; i++) {
+    const square = document.createElement("div");
+
+    square.setAttribute("class", "square");
+    square.style.minHeight = "30px";
+    square.style.minWidth = "30px";
+
+    row.appendChild(square);
+  }
 }
